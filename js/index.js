45379b0f938ab01423539aea398eb2c3bd32bbec
liuabaao <ln960523@163.com>
2017-11-17 11:45:59 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -45,9 +45,137 @@ $(function(){
 	})
 	
 	
+//显
+	$.getJSON("json/content.json",function(data){
+			$.each(data,function(index,value){
+				$(".plate-a").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
+			})
+		})
+
+$.getJSON("json/content1.json",function(data){
+			$.each(data,function(index,value){
+				$(".plate-b").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
+			})
+		})
+
+
+$.getJSON("json/content2.json",function(data){
+			$.each(data,function(index,value){
+				$(".plate-c").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
+			})
+		})
+
+	var end = new Date("2017-11-20 00:00:00");
+	var start = new Date();
+	var t = diff(start,end);//秒
+	var oP = document.getElementsByClassName("D")[0];
+	function showTime(){
+		//剩余的小时数
+		var h = parseInt( t / 3600 ) ;
+		//剩余的分钟数
+		var m = parseInt( (t - h*3600) / 60 ) ;
+		//剩余的秒数
+		var s = parseInt( t - h*3600 - m * 60 );
+		oP.innerHTML = "距开始:" + h + "小时" + m + "分钟" + s + "秒";	
+		return "距开始:" + h + "小时" + m + "分钟" + s + "秒";
+		
+		oA.innerHTML = "距开始:" + h + "小时" + m + "分钟" + s + "秒";	
+		return "距开始:" + h + "小时" + m + "分钟" + s + "秒";
+	}
+	//页面加载时显示时间 防止时间显示延时问题
+	oP.innerHTML = showTime();
+	
+	var timer = setInterval(function (){
+		t--;
+		if( t <= 0 ){
+			oP.innerHTML = "抱歉该商品下架了";
+			clearInterval(timer);
+		}else{
+			showTime();
+		}
+	} , 1000)
 	
 	
 	
 	
+	//下logo
 	
+	$.getJSON("json/cont.json",function(data){
+			$.each(data,function(index,value){
+				$(".logo").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
+			})
+		})
+	
+	//wrp
+	
+	
+	$.getJSON("json/wrp.json",function(data){
+			$.each(data,function(index,value){
+				$(".wrp").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
+			})
+		})
+	
+	
+	//pB
+	$.getJSON("json/cont.json",function(data){
+			$.each(data,function(index,value){
+				$(".pR").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
+			})
+})
+	
+	
+	//pB
+	var str ='';
+	$.ajax("json/wman.json")
+	
+	.then(function(res){
+		for(var i = 0;i < 6;i++){
+			str += `<li>
+						<a href="#">
+							<img src=${res[i].img}/>
+						</a>
+					</li>`;
+		}
+	$(".pLate").html(str);
+	})
+	
+	var arr ='';
+	$.ajax("json/wman.json")
+	
+	.then(function(res){
+		for(var i = 6;i < 12;i++){
+			arr += `<li>
+						<a href="#">
+							<img src=${res[i].img}/>
+						</a>
+					</li>`;
+		}
+	$(".bgR").html(arr);
+	})
+	
+	
+	
+	
+	var acc ='';
+	$.ajax("json/wman.json")
+	
+	.then(function(res){
+		for(var i = 12;i < 18;i++){
+			acc += `<li>
+						<a href="#">
+							<img src=${res[i].img}/>
+						</a>
+					</li>`;
+		}
+	$(".qR").html(acc);
+	})
+	
+	
+	
+	$("body img").mouseover(function(){
+		$(this).animate({"opacity":"0.4"})
+	})
+	$("body img").mouseout(function(){
+		$(this).animate({"opacity":"1"})
+	})
 })
--- js/xl.js
+++ js/xl.js
@@ -4,4 +4,17 @@ $(function(){
 				$(".xL").append(`<li style="z-index:${value.index}"><a href="#" ><img src=${value.img}/></a></li>`);
 			})
 		})
+		
+		
+		$(".recR").click(function(){
+			$(this).animate({"left":"1160"})
+			$(".xL").animate({"left":"-1175"},function(){
+				$(".recL").click(function(){
+					$(this).animate({"left":"0"})
+					$(".xL").animate({"left":"0"})
+				})
+			})
+			
+		})
+		
 })
